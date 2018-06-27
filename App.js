d8ea0f827c773802189dd339703713acd12c4a3d
Craig Orcutt <craigaorcutt@gmail.com>
2018-06-26 22:45:22 -0500

    added packages necessary for graphql queries

--- App.js
+++ App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { setContext } from "apollo-link-context";
+import { getToken } from "./loginUtils";
+import graphKey  from "./graphKey";
+import Navigator from './Navigator';
 
+// sets context for apollo link
+const authLink = setContext(async (req, { headers }) => {
+  const token = await getToken();
+
+  // sets authorization headers for JWT
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null
+    }
+  };
+});
+
+// creates link to graphCool
+const httpLink = new HttpLink({
+  uri: `${graphKey.key}`
+});
+
+// links app to graphCool
+const link = authLink.concat(httpLink);
+
+// creates new apollo client
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <Navigator />
+      </ApolloProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
