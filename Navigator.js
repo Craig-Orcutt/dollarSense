ac08c752b25f47ed5fbd6eed7cccb503d8f570a3
Craig Orcutt <craigaorcutt@gmail.com>
2018-06-26 22:45:59 -0500

    started basic layout of navigation stack for app

---
+++ Navigator.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { createStackNavigator } from "react-navigation";
+import { graphql, withApollo } from "react-apollo";
+import gql from "graphql-tag";
+import { StyleSheet, View, ActivityIndicator, Button } from "react-native";
+import Post from "./components/money/Post";
+import Posts from "./components/money/Posts";
+import NewPost from "./components/money/NewPost";
+import Login from "./components/user/Login";
+import UpdatePost from "./components/money/EditPost";
+import { Fab, Icon } from "native-base";
+import navigationStyles from "./styles/navigationStyles";
+import { signOut } from "./loginUtils";
+
+class Home extends React.Component {
+  static navigationOptions = {
+    title: "Home",
+    ...navigationStyles
+  };
+
+
+  // navigation once logged in. either create new post or view old money
+  goToPost = () => {
+    this.props.navigation.navigate("Post");
+  };
+  addNewPost = () => {
+    this.props.navigation.navigate("NewPost");
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Posts {...this.props} />
+        <Button
+          title="LogOut"
+          onPress={() => {
+            signOut();
+            this.props.client.resetStore;
+          }}
+        />
+        <Fab style={styles.newPost} onPress={this.addNewPost}>
+          <Icon name="add" />
+        </Fab>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between"
+  },
+  newPost: {
+    backgroundColor: "#82D8D8"
+  },
+  newPostText: {
+    fontSize: 20,
+    textAlign: "center"
+  }
+});
+
+// creates navigator stack that I can then use the navigate prop to get to the proper screen
+const Navigator = createStackNavigator({
+  Home: {
+    screen: withApollo(Home)
+  },
+  Post: {
+    screen: Post
+  },
+  NewPost: {
+    screen: NewPost
+  },
+  UpdatePost: {
+    screen: UpdatePost
+  }
+});
+
+
+// wrapper that is exported basically as the root of the app. if not logged in, user must login or register
+const NavWrapper = ({ loading, user }) => {
+  if (loading) return <ActivityIndicator size="large" />;
+  if (!user) return <Login />;
+  return <Navigator screenProps={{ user }} />;
+};
+
+// graphql to get all posts by a user
+const userQuery = gql`
+  query userQuery {
+    user {
+      id
+      email
+      posts(orderBy: createdAt_DESC) {
+        id
+        title
+      }
+    }
+  }
+`;
+
+
+export default graphql(userQuery, {
+  props: ({ data }) => ({ ...data })
+})(NavWrapper);
