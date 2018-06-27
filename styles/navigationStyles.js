2eb679cd4200e048a5f3a9f3f9b21b5e8417c0f3
Craig Orcutt <craigaorcutt@gmail.com>
2018-06-26 22:47:51 -0500

    basic navigation styles

---
+++ styles/navigationStyles.js
@@ -0,0 +1,14 @@
+export default {
+
+  headerStyle: {
+    backgroundColor: '#373142',
+  },
+  headerTitleStyle: {
+    color: '#FFF'
+  },
+  headerBackTitleStyle: {
+    color: "#82D8D8"
+  },
+  headerTintColor: "#82D8D8"
+
+}
